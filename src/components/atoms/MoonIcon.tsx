36765c6be44978c4406f5fd8df685f9642e2e95b
Dobromir Yordanov <32433021+dobromiryor@users.noreply.github.com>
2024-08-21 14:58:56 +0300

    fix: 🐛 fixed easing

--- src/components/atoms/MoonIcon.tsx
+++ src/components/atoms/MoonIcon.tsx
@@ -2,87 +2,88 @@ import clsx from "clsx";
 import { useMemo } from "react";
 
 interface MoonIconProps {
-  lat: number;
-  phase: number;
+	lat: number;
+	phase: number;
 }
 
-const ease = (x: number) => 1 - Math.sqrt(1 - Math.pow(x, 2));
+const easeOutExponential = (x: number) => 1 - Math.pow(1 - x, 2.5);
+
+const easeInOutQuad = (x: number) =>
+	x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
 
 export const MoonIcon = ({ lat, phase }: MoonIconProps) => {
-  const isGibbous = useMemo(() => phase >= 0.25 && phase < 0.75, [phase]);
+	const isGibbous = useMemo(() => phase >= 0.25 && phase < 0.75, [phase]);
 
-  const scale = useMemo(() => {
-    const min = 1;
-    const max = 4;
+	const scale = useMemo(() => {
+		const min = 1;
+		const max = 4;
 
-    if (phase < 0.25) {
-      return min + (phase / 0.25) * (max - min);
-    } else if (phase < 0.5) {
-      return max - ((phase - 0.25) / 0.25) * (max - min);
-    } else if (phase < 0.75) {
-      return min + ((phase - 0.5) / 0.25) * (max - min);
-    } else {
-      return max - ((phase - 0.75) / 0.25) * (max - min);
-    }
-  }, [phase]);
+		if (phase < 0.25) {
+			return min + easeInOutQuad(phase / 0.25) * (max - min);
+		} else if (phase < 0.5) {
+			return max - easeOutExponential((phase - 0.25) / 0.25) * (max - min);
+		} else if (phase < 0.75) {
+			return min + easeInOutQuad((phase - 0.5) / 0.25) * (max - min);
+		} else {
+			return max - easeOutExponential((phase - 0.75) / 0.25) * (max - min);
+		}
+	}, [phase]);
 
-  const translation = useMemo(() => {
-    if (phase < 0.25) {
-      const min = 32;
-      const max = 36;
+	const translation = useMemo(() => {
+		if (phase < 0.25) {
+			const min = 32;
+			const max = 36;
 
-      return min + (phase / 0.25) * (max - min);
-    } else if (phase < 0.5) {
-      const min = 64;
-      const max = 36;
+			return min + easeInOutQuad(phase / 0.25) * (max - min);
+		} else if (phase < 0.5) {
+			const min = 64;
+			const max = 36;
 
-      return max - ease((phase - 0.25) / 0.25) * (max - min);
-    } else if (phase < 0.75) {
-      const min = 64;
-      const max = 68;
+			return max - easeInOutQuad((phase - 0.25) / 0.25) * (max - min);
+		} else if (phase < 0.75) {
+			const min = 64;
+			const max = 68;
 
-      return min + ((phase - 0.5) / 0.25) * (max - min);
-    } else {
-      const min = 96;
-      const max = 68;
+			return min + easeInOutQuad((phase - 0.5) / 0.25) * (max - min);
+		} else {
+			const min = 96;
+			const max = 68;
 
-      return max - ease((phase - 0.75) / 0.25) * (max - min);
-    }
-  }, [phase]);
+			return max - easeInOutQuad((phase - 0.75) / 0.25) * (max - min);
+		}
+	}, [phase]);
 
-  const maskStyles = "w-8 h-8 rounded-full";
+	const maskStyles = "w-8 h-8 rounded-full";
 
-  return (
-    <div
-      className="relative flex justify-center items-center h-8 w-8 rounded-full pointer-events-none overflow-hidden origin-center"
-      style={{ rotate: `-${90 - lat}deg` }}
-    >
-      {/* Background */}
-      <div
-        className={clsx(
-          "h-8 w-8 rounded-full transition-all",
-          !isGibbous ? "bg-zinc-700" : "bg-primary"
-        )}
-      />
+	return (
+		<div
+			className="relative flex justify-center items-center h-8 w-8 rounded-full pointer-events-none overflow-hidden origin-center"
+			style={{ rotate: `-${90 - lat}deg` }}>
+			{/* Background */}
+			<div
+				className={clsx(
+					"h-8 w-8 rounded-full transition-all",
+					!isGibbous ? "bg-zinc-700" : "bg-primary"
+				)}
+			/>
 
-      <div
-        className="absolute left-8 flex origin-left transition-all"
-        style={{
-          transform: `scale(${scale}) translateX(${-translation}px)`,
-        }}
-      >
-        {/* Right mask */}
-        <div className={clsx(maskStyles, "bg-primary")} />
+			<div
+				className="absolute left-8 flex origin-left transition-all"
+				style={{
+					transform: `scale(${scale}) translateX(${-translation}px)`,
+				}}>
+				{/* Right mask */}
+				<div className={clsx(maskStyles, "bg-primary")} />
 
-        {/* Center mask */}
-        <div className={clsx(maskStyles, "bg-zinc-700")} />
+				{/* Center mask */}
+				<div className={clsx(maskStyles, "bg-zinc-700")} />
 
-        {/* Left mask */}
-        <div className={clsx(maskStyles, "bg-primary")} />
-      </div>
+				{/* Left mask */}
+				<div className={clsx(maskStyles, "bg-primary")} />
+			</div>
 
-      {/* Shadow overlay */}
-      <div className="absolute w-8 h-8 rounded-full shadow-center-inset-2 shadow-primary" />
-    </div>
-  );
+			{/* Shadow overlay */}
+			<div className="absolute w-8 h-8 rounded-full shadow-center-inset-2 shadow-primary" />
+		</div>
+	);
 };
